963d15bd39e810e5f2c41190d5513284033360f4
NekoYasha <autornekoyasha@gmail.com>
2023-05-07 10:51:55 -0300

    created author.java

--- src/main/java/com/mynovellist/back/novel/author/model/Author.java
+++ src/main/java/com/mynovellist/back/novel/author/model/Author.java
@@ -1,2 +1,29 @@
-package com.mynovellist.back.novel.author.model;public class Author {
+//=============================================={ PACKAGE }==============================================//
+package com.mynovellist.back.novel.author.model;
+//=============================================={ END PACKAGE }==============================================//
+
+//=============================================={ IMPORTS }==============================================//
+import com.mynovellist.back.insfrastructure.model.AbstractEntity;
+
+import com.mynovellist.back.novel.model.Novel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.List;
+//=============================================={ END IMPORTS }==============================================//
+
+@Entity
+@Table(name="authors")
+@Data
+public class Author extends AbstractEntity {
+
+    //=============================================={ ATTRIBUTES }==============================================//
+    private String name;
+    private String twitterUsername;
+    private String email;
+
+    @OneToMany(mappedBy = "author")
+    List<Novel> novels;
 }
